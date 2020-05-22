2bff0d2e9698014c8ebd45515ec84d63d7e75b3d
twostone <revelation0072@gmail.com>
2020-05-22 09:07:24 +0900

    posts 조회

--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -2,16 +2,21 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <ul>
-        <li v-for="post in posts" :key="post._id">
-          {{ post.title }}
-        </li>
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+      <ul v-else>
+        <PostListItem
+          v-for="post in posts"
+          :key="post._id"
+          :postItem="post"
+        ></PostListItem>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import PostListItem from '@/components/posts/PostListItem.vue';
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue';
 import { fetchPosts } from '@/api/index';
 
 export default {
@@ -19,17 +24,26 @@ export default {
     return {
       posts: [],
       logMessage: '',
+      isLoading: false,
     };
   },
   methods: {
     async getPosts() {
+      this.isLoading = true;
       try {
         const { data } = await fetchPosts();
         this.posts = data.posts;
+        console.log(this.posts);
       } catch (error) {
         this.logMessage = error.response.data;
+      } finally {
+        this.isLoading = false;
       }
     },
+  },
+  components: {
+    PostListItem,
+    LoadingSpinner,
   },
   created() {
     this.getPosts();
