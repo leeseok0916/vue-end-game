f6388dc13764447a3321b0b697668147cf5595e1
twostone <revelation0072@gmail.com>
2020-05-25 19:46:07 +0900

    api refactoring

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -2,30 +2,20 @@ import axios from 'axios';
 import { setInterceptor } from './common/interceptor';
 
 function createInstance() {
-  const instance = axios.create({
+  return axios.create({
     baseURL: process.env.VUE_APP_API_URL,
   });
+}
 
+// 액시오스 초기화 함수
+function createInstanceWithAuth(url) {
+  const instance = axios.create({
+    baseURL: `${process.env.VUE_APP_API_URL}${url}`,
+  });
   return setInterceptor(instance);
 }
 
 const instance = createInstance();
+const posts = createInstanceWithAuth('posts');
 
-function registerUser(userData) {
-  return instance.post('signup', userData);
-}
-
-function loginUser(loginData) {
-  return instance.post('login', loginData);
-}
-
-function fetchPosts() {
-  return instance.get('posts');
-}
-
-// 게시물을 한개 생성하는 API
-const createPost = postData => {
-  return instance.post('posts', postData);
-};
-
-export { registerUser, loginUser, fetchPosts, createPost };
+export { instance, posts };
